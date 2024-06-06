abdec19d717f71f6f309784c216ebe5298be7f9d
VladD16 <162788325+VladD16@users.noreply.github.com>
2024-06-06 16:15:54 +0300

    Add files via upload

---
+++ lesson_21/js/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// let numOne = 20;
+// let numTwo = "20";
+// let numSumm = numOne + numTwo;
+// console.log(numSumm);
+
+// let userName = "Vlad";
+// console.log(userName);
+// userName = "Olga";
+// console.log(userName);
+
+// const userName = "Vlad";
+// userName = "Olga";
+// console.log(userName);
+
+// console.log(typeof numOne);
+// console.log(typeof userName);
+// console.log(typeof numTwo);
+
+// console.log(`My name is ${userName}. I am ${userAge} years old.`);
+
+// let someString = "Hi, world!";
+// console.log(someString.length);
+// console.log(someString[2]);
+// console.log(someString.toLowerCase());
+// console.log(someString.toUpperCase());
+// console.log(someString.includes("world"));
+// console.log(someString.startsWith("Hi"));
+// console.log(someString.endsWith("D!"));
+
+// let num = 20.25;
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+// console.log(Math.round(num));
